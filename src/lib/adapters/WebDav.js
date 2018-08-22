6a450bb2928fa31470b9928513968d49cdba0c3a
Marcel Klehr <mklehr@gmx.net>
2018-08-22 18:16:18 +0200

    WebDAV: Use utf8 encoding

--- src/lib/adapters/WebDav.js
+++ src/lib/adapters/WebDav.js
@@ -233,7 +233,7 @@ export default class WebDavAdapter extends CachingAdapter {
   }
 
   createXBEL(myTopFolder) {
-    let output = `<?xml version="1.0" encoding="ISO-8859-1"?>
+    let output = `<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE xbel PUBLIC "+//IDN python.org//DTD XML Bookmark Exchange Language 1.0//EN//XML" "http://www.python.org/topics/xml/dtds/xbel-1.0.dtd">
 <xbel version="1.0">
 `
