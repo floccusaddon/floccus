25b8b0b81ebc697724b7407352d4b75b896d7116
Marcel Klehr <mklehr@gmx.net>
2019-07-26 17:11:34 +0200

    Test reporter: Add test summary at the end for quick overview

--- src/test/reporter.js
+++ src/test/reporter.js
@@ -9,33 +9,43 @@ const {
 export function createWebdriverAndHtmlReporter(html_reporter) {
   return function(runner) {
     Mocha.reporters.Base.call(this, runner)
+
+    // report on the selenium screen, too
     new html_reporter(runner)
 
-    // Scroll down test display after each test
+    // build a summary
+    const summary = []
+
     let mocha = document.querySelector('#mocha')
     runner.on(EVENT_TEST_BEGIN, test => {
       resetKillTimeout()
       console.log('\n### ' + test.title + ' ###\n')
+      // Scroll down test display after each test
       mocha.scrollTop = mocha.scrollHeight
     })
 
     runner.on(EVENT_SUITE_BEGIN, suite => {
       if (suite.root) return
       console.log('\n## ' + suite.title + ' ## \n')
+      summary.push('## ' + suite.title)
     })
 
     runner.on(EVENT_TEST_FAIL, test => {
       console.log('->', 'FAILED :', test.title, stringifyException(test.err))
+      summary.push(this.symbols.err, test.title)
       resetKillTimeout()
     })
     runner.on(EVENT_TEST_PASS, test => {
       console.log('->', 'PASSED :', test.title, test.duration / 1000 + 's')
+      summary.push(this.symbols.ok, test.title)
       resetKillTimeout()
     })
 
     runner.on(EVENT_RUN_END, () => {
       var minutes = Math.floor(runner.stats.duration / 1000 / 60)
       var seconds = Math.round((runner.stats.duration / 1000) % 60)
+
+      console.log('\n' + summary.join('\n'))
 
       console.log(
         'FINISHED ' + (runner.stats.failures > 0 ? 'FAILED' : 'PASSED') + ' -',
