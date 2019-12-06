a652ea972cd3f218448e30e0e1dd00f6141f963d
Marcel Klehr <mklehr@gmx.net>
2019-12-06 23:22:49 +0100

    Tests: remove kill timeout

--- src/test/reporter.js
+++ src/test/reporter.js
@@ -18,7 +18,6 @@ export function createWebdriverAndHtmlReporter(html_reporter) {
 
     let mocha = document.querySelector('#mocha')
     runner.on(EVENT_TEST_BEGIN, test => {
-      resetKillTimeout()
       console.log('\n### ' + test.title + ' ###\n')
       // Scroll down test display after each test
       mocha.scrollTop = mocha.scrollHeight
@@ -33,13 +32,11 @@ export function createWebdriverAndHtmlReporter(html_reporter) {
     runner.on(EVENT_TEST_FAIL, test => {
       console.log('->', 'FAILED :', test.title, stringifyException(test.err))
       summary.push(Mocha.reporters.Base.symbols.err + ' ' + test.title)
-      resetKillTimeout()
     })
     runner.on(EVENT_TEST_PASS, test => {
       const status = `${test.title} (${test.duration / 1000}s)`
       console.log('->', 'PASSED :', status)
       summary.push(Mocha.reporters.Base.symbols.ok + ' ' + status)
-      resetKillTimeout()
     })
 
     runner.on(EVENT_RUN_END, () => {
@@ -59,16 +56,6 @@ export function createWebdriverAndHtmlReporter(html_reporter) {
   }
 }
 
-var killTimeout
-function resetKillTimeout() {
-  if (killTimeout) clearTimeout(killTimeout)
-  killTimeout = setTimeout(() => {
-    console.log(
-      'FINISHED FAILED - no test has ended for 3 minutes, tests stopped'
-    )
-  }, 60000 * 3)
-}
-
 function stringifyException(exception) {
   var err = exception.stack || exception.toString()
 
@@ -79,7 +66,7 @@ function stringifyException(exception) {
 
   // <=IE7 stringifies to [Object Error]. Since it can be overloaded, we
   // check for the result of the stringifying.
-  if ('[object Error]' == err) err = exception.message
+  if (err == '[object Error]') err = exception.message
 
   // Safari doesn't give you a stack. Let's at least provide a source line.
   if (!exception.stack && exception.sourceURL && exception.line !== undefined) {
