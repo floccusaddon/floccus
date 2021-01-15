cf6410f2ed82f36da78825275957c532108e8ae0
Marcel Klehr <mklehr@gmx.net>
2021-01-15 10:00:25 +0100

    Tests: Sync twice when reconciling fuzzed deletions to mitigate consistency problems

--- src/test/test.js
+++ src/test/test.js
@@ -3760,6 +3760,11 @@ describe('Floccus', function() {
 
             await account2.sync()
             expect(account2.getData().error).to.not.be.ok
+
+            // Sync twice, because some removal-move mixes are hard to sort out consistently
+            await account2.sync()
+            expect(account2.getData().error).to.not.be.ok
+
             console.log('second round: account2 completed')
 
             const serverTreeAfterSecondSync = await getAllBookmarks(account1)
