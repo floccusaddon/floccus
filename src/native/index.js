a8916833e06ded3b1776a0d9b1843ee5bd5f4734
Marcel Klehr <mklehr@gmx.net>
2021-04-14 15:34:41 +0200

    Remove nativescript

--- src/native/index.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'nativescript-vue'
-import App from './App'
-
-// Vue.config.debug = true
-// Vue.config.silent = false
-new Vue({
-  render: (h) => h('frame', [h(App)]),
-}).$start()
--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-const webpack = require('@nativescript/webpack')
-
-module.exports = (env) => {
-  webpack.init(env)
-
-  return webpack.resolveConfig()
-}
