7d36e030c140ecfba67960c2926517a2c7897b45
Marcel Klehr <mklehr@gmx.net>
2021-07-14 11:14:21 +0200

    Fix UnidirectionalMerge: Allow reorders
    Fixes #884

--- src/lib/strategies/UnidirectionalMerge.ts
+++ src/lib/strategies/UnidirectionalMerge.ts
@@ -1,8 +1,6 @@
 import Diff from '../Diff'
 import Unidirectional from './Unidirectional'
 import MergeSyncProcess from './Merge'
-import { OrderFolderResource } from '../interfaces/Resource'
-import Logger from '../Logger'
 
 export default class UnidirectionalMergeSyncProcess extends Unidirectional {
   async getDiffs():Promise<{localDiff:Diff, serverDiff:Diff}> {
@@ -11,9 +9,5 @@ export default class UnidirectionalMergeSyncProcess extends Unidirectional {
 
   async loadChildren() :Promise<void> {
     this.serverTreeRoot = await this.server.getBookmarksTree(true)
-  }
-
-  async executeReorderings(resource:OrderFolderResource, reorderings:Diff):Promise<void> {
-    Logger.log('Skipping reorderings because this is a merge process ',{ reorderings })
   }
 }
