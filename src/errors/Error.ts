a65556eddc2b8b23bae9a185169efa36e5f42a53
Marcel Klehr <mklehr@gmx.net>
2021-11-11 20:59:24 +0100

    Fix Error class inheritance

--- src/errors/Error.ts
+++ src/errors/Error.ts
@@ -1,11 +1,18 @@
 export class FloccusError extends Error {
   public code: number
+
+  constructor(message) {
+    super(message)
+    // See https://stackoverflow.com/questions/12915412/how-do-i-extend-a-host-object-e-g-error-in-typescript#22666069
+    Object.setPrototypeOf(this, FloccusError.prototype)
+  }
 }
 
 export class UnknownCreateTargetError extends FloccusError {
   constructor() {
     super("E001: Folder to create in doesn't exist")
     this.code = 1
+    Object.setPrototypeOf(this, UnknownCreateTargetError.prototype)
   }
 }
 
@@ -13,6 +20,7 @@ export class UnknownBookmarkUpdateError extends FloccusError {
   constructor() {
     super("E002: Bookmark to update doesn't exist anymore")
     this.code = 2
+    Object.setPrototypeOf(this, UnknownBookmarkUpdateError.prototype)
   }
 }
 
@@ -20,6 +28,7 @@ export class UnknownMoveOriginError extends FloccusError {
   constructor() {
     super("E003: Folder to move out of doesn't exist")
     this.code = 3
+    Object.setPrototypeOf(this, UnknownMoveOriginError.prototype)
   }
 }
 
@@ -27,6 +36,7 @@ export class UnknownMoveTargetError extends FloccusError {
   constructor() {
     super("E004: Folder to move into doesn't exist")
     this.code = 4
+    Object.setPrototypeOf(this, UnknownMoveTargetError.prototype)
   }
 }
 
@@ -34,6 +44,7 @@ export class UnknownFolderParentUpdateError extends FloccusError {
   constructor() {
     super("E006: Parent of folder to update doesn't exist")
     this.code = 5
+    Object.setPrototypeOf(this, UnknownFolderParentUpdateError.prototype)
   }
 }
 
@@ -41,6 +52,7 @@ export class UnknownFolderUpdateError extends FloccusError {
   constructor() {
     super("E006: Folder to update doesn't exist")
     this.code = 6
+    Object.setPrototypeOf(this, UnknownFolderUpdateError.prototype)
   }
 }
 
@@ -48,6 +60,7 @@ export class UnknownFolderMoveError extends FloccusError {
   constructor() {
     super("E007: Folder to move doesn't exist")
     this.code = 7
+    Object.setPrototypeOf(this, UnknownFolderMoveError.prototype)
   }
 }
 
@@ -58,6 +71,7 @@ export class UnknownFolderOrderError extends FloccusError {
   constructor() {
     super('E010: Could not find folder to order')
     this.code = 10
+    Object.setPrototypeOf(this, UnknownFolderOrderError.prototype)
   }
 }
 
@@ -67,6 +81,7 @@ export class UnknownFolderItemOrderError extends FloccusError {
     super('E011: Item in folder ordering is not an actual child')
     this.code = 11
     this.item = item
+    Object.setPrototypeOf(this, UnknownFolderItemOrderError.prototype)
   }
 }
 
@@ -76,6 +91,7 @@ export class MissingItemOrderError extends FloccusError {
     super("E012: Folder ordering is missing some of the folder's children")
     this.code = 12
     this.item = item
+    Object.setPrototypeOf(this, MissingItemOrderError.prototype)
   }
 }
 
@@ -83,6 +99,7 @@ export class UnknownFolderRemoveError extends FloccusError {
   constructor() {
     super("E013: Folder to remove doesn't exist")
     this.code = 13
+    Object.setPrototypeOf(this, UnknownFolderRemoveError.prototype)
   }
 }
 
@@ -90,6 +107,7 @@ export class UnknownFolderParentRemoveError extends FloccusError {
   constructor() {
     super("E014: Parent folder to remove folder from of doesn't exist")
     this.code = 14
+    Object.setPrototypeOf(this, UnknownFolderParentRemoveError.prototype)
   }
 }
 
@@ -97,6 +115,7 @@ export class UnexpectedServerResponseError extends FloccusError {
   constructor() {
     super('E015: Unexpected response data from server')
     this.code = 15
+    Object.setPrototypeOf(this, UnexpectedServerResponseError.prototype)
   }
 }
 
@@ -104,6 +123,7 @@ export class RequestTimeoutError extends FloccusError {
   constructor() {
     super('E016: Request timed out.')
     this.code = 16
+    Object.setPrototypeOf(this, RequestTimeoutError.prototype)
   }
 }
 
@@ -111,6 +131,7 @@ export class NetworkError extends FloccusError {
   constructor() {
     super('E017: Network error: Check your network connection and your account details')
     this.code = 17
+    Object.setPrototypeOf(this, NetworkError.prototype)
   }
 }
 
@@ -118,6 +139,7 @@ export class AuthenticationError extends FloccusError {
   constructor() {
     super("E018: Couldn't authenticate with the server.")
     this.code = 18
+    Object.setPrototypeOf(this, AuthenticationError.prototype)
   }
 }
 
@@ -129,6 +151,7 @@ export class HttpError extends FloccusError {
     this.code = 19
     this.status = status
     this.method = method
+    Object.setPrototypeOf(this, HttpError.prototype)
   }
 }
 
@@ -138,6 +161,7 @@ export class ParseResponseError extends FloccusError {
     super('E020: Could not parse server response. Is the bookmarks app installed on your server?')
     this.code = 20
     this.response = response
+    Object.setPrototypeOf(this, ParseResponseError.prototype)
   }
 }
 
@@ -145,6 +169,7 @@ export class InconsistentServerStateError extends FloccusError {
   constructor() {
     super('E021: Inconsistent server state. Folder is present in childorder list but not in folder tree')
     this.code = 21
+    Object.setPrototypeOf(this, InconsistentServerStateError.prototype)
   }
 }
 
@@ -156,6 +181,7 @@ export class InconsistentBookmarksExistenceError extends FloccusError {
     this.code = 22
     this.folder = folder
     this.bookmark = bookmark
+    Object.setPrototypeOf(this, InconsistentBookmarksExistenceError.prototype)
   }
 }
 
@@ -166,6 +192,7 @@ export class UnclearedLockFileError extends FloccusError {
     super(`E023: Unable to clear lock file, consider deleting ${lockFile} manually.`)
     this.code = 23
     this.lockFile = lockFile
+    Object.setPrototypeOf(this, UnclearedLockFileError.prototype)
   }
 }
 
@@ -178,6 +205,7 @@ export class LockFileError extends FloccusError {
     this.code = 24
     this.status = status
     this.lockFile = lockFile
+    Object.setPrototypeOf(this, LockFileError.prototype)
   }
 }
 
@@ -188,6 +216,7 @@ export class SlashError extends FloccusError {
   constructor() {
     super("E025: Bookmarks file setting mustn't begin with a slash: '/'")
     this.code = 25
+    Object.setPrototypeOf(this, SlashError.prototype)
   }
 }
 
@@ -200,6 +229,7 @@ export class InterruptedSyncError extends FloccusError {
   constructor() {
     super('E027: Sync process was interrupted')
     this.code = 27
+    Object.setPrototypeOf(this, InterruptedSyncError.prototype)
   }
 }
 
@@ -212,6 +242,7 @@ export class FailsafeError extends FloccusError {
     super(`E029: Failsafe: The current sync run would delete ${percent}% of your bookmarks. Refusing to execute. Disable this failsafe in the account settings if you want to proceed anyway.`)
     this.code = 29
     this.percent = percent
+    Object.setPrototypeOf(this, FailsafeError.prototype)
   }
 }
 
@@ -219,6 +250,7 @@ export class DecryptionError extends FloccusError {
   constructor() {
     super('E030: Failed to decrypt bookmarks file. The passphrase may be wrong or the file may be corrupted.')
     this.code = 30
+    Object.setPrototypeOf(this, DecryptionError.prototype)
   }
 }
 
@@ -226,6 +258,7 @@ export class GoogleDriveAuthenticationError extends FloccusError {
   constructor() {
     super('E031: Could not authenticate with Google Drive. Please connect floccus with your google account again.')
     this.code = 31
+    Object.setPrototypeOf(this, GoogleDriveAuthenticationError.prototype)
   }
 }
 
@@ -233,13 +266,15 @@ export class OAuthTokenError extends FloccusError {
   constructor() {
     super('E032: OAuth error. Token validation error. Please reconnect your Google Account.')
     this.code = 32
+    Object.setPrototypeOf(this, OAuthTokenError.prototype)
   }
 }
 
 export class RedirectError extends FloccusError {
   constructor() {
-    super("E033: Redirect detected. Please install the Bookmarks app on your nextcloud and make sure the nextcloud URL you entered doesn't redirect to a different location.")
+    super("E033: Redirect detected. Please make sure the server supports the selected sync method and URL you entered is correct doesn't redirect to a different location.")
     this.code = 33
+    Object.setPrototypeOf(this, RedirectError.prototype)
   }
 }
 
@@ -247,5 +282,6 @@ export class FileUnreadableError extends FloccusError {
   constructor() {
     super('E034: Bookmarks file is unreadable. Did you forget to set an encryption passphrase?')
     this.code = 34
+    Object.setPrototypeOf(this, FileUnreadableError.prototype)
   }
 }
