2bd9441b0a302b7b7c8da1d8e7976340ba0156c4
binsee <5285894+binsee@users.noreply.github.com>
2022-02-04 15:28:57 +0800

    Fix get languages on native

--- src/ui/native.js
+++ src/ui/native.js
@@ -7,26 +7,21 @@ import store from './store/native'
 import i18nPlugin from './plugins/i18n'
 import { router } from './NativeRouter'
 import {i18n} from '../lib/native/I18n'
-import { Device } from '@capacitor/device'
 
 Vue.mixin(i18nPlugin)
 Vue.mixin(capacitor)
 
 const app = () => {
-  Device.getLanguageCode()
-    .then(({ value }) => {
-      i18n.setLocales([value])
-      return i18n.load()
+  i18n.setLocales(navigator.languages)
+  i18n.load().then(() => {
+    window['floccus'] = global['Floccus'] = new Vue({
+      el: '#app',
+      router,
+      store,
+      vuetify,
+      render: (h) => h(App),
     })
-    .then(() => {
-      window['floccus'] = global['Floccus'] = new Vue({
-        el: '#app',
-        router,
-        store,
-        vuetify,
-        render: (h) => h(App),
-      })
-    })
+  })
 }
 
 export default app
