3950d4503168a7f86f757fa0f8a2d5ecb9ad134b
Marcel Klehr <mklehr@gmx.net>
2022-03-15 12:24:58 +0100

    Tests: Use hashing for testing old APIs

--- src/test/test.js
+++ src/test/test.js
@@ -204,7 +204,6 @@ describe('Floccus', function() {
             }
             await account.init()
             if (ACCOUNT_DATA.type === 'nextcloud-bookmarks' && ACCOUNT_DATA.oldAPIs) {
-              account.server.hasFeatureHashing = false
               account.server.hasFeatureChildren = false
             }
             if (ACCOUNT_DATA.noCache) {
@@ -4010,8 +4009,6 @@ describe('Floccus', function() {
             account2.server.__defineGetter__('highestId', () => account1.server.highestId)
           }
           if (ACCOUNT_DATA.type === 'nextcloud-bookmarks' && ACCOUNT_DATA.oldAPIs) {
-            account1.server.hasFeatureHashing = false
-            account2.server.hasFeatureHashing = false
             account1.server.hasFeatureChildren = false
             account2.server.hasFeatureChildren = false
           }
