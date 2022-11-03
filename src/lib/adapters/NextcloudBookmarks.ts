f2a4c3dfe039664ebb47a4b12be55ef45828f6d6
Marcel Klehr <mklehr@gmx.net>
2022-11-03 21:10:21 +0100

    NextcloudBookmarks: Report all statuses > 400 as HttpError
    fixes #1277

--- src/lib/adapters/NextcloudBookmarks.ts
+++ src/lib/adapters/NextcloudBookmarks.ts
@@ -898,7 +898,7 @@ export default class NextcloudBookmarksAdapter implements Adapter, BulkImportRes
     if (res.status === 401 || res.status === 403) {
       throw new AuthenticationError()
     }
-    if (res.status === 503) {
+    if (res.status === 503 || res.status > 400) {
       throw new HttpError(res.status, verb)
     }
     let json
