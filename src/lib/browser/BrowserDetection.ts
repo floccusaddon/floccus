3dac93350f11568e4aebbcce3b4dcb7efd1a8937
Marcel Klehr <mklehr@gmx.net>
2024-03-09 20:22:36 +0100

    fix: Android app stuck on splash screen
    fixes #1557
    
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- src/lib/browser/BrowserDetection.ts
+++ src/lib/browser/BrowserDetection.ts
@@ -1,6 +1,5 @@
-import browser from '../browser-api'
-
 export const isVivaldi = async() => {
-  const tabs = await browser.tabs.query({ active: true, currentWindow: true });
+  const {default: browser} = await import('../browser-api.js')
+  const tabs = await browser.tabs.query({ active: true, currentWindow: true })
   return Boolean(tabs?.[0]?.['vivExtData'])
 }
