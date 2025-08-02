d914cbb1e5d4aeaca003d8a7582adca5fe29f74f
yougotwill <yougotwill94@gmail.com>
2025-08-02 13:16:58 +0200

    feat: make newAccount native view so we can use native scroll
    it wraps around the standard view so we don't  break the web extension scrolling

---
+++ src/ui/views/native/NewAccount.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="native-scroll-container">
+    <v-main>
+      <NewAccountContent />
+    </v-main>
+  </div>
+</template>
+
+<script>
+import NewAccountContent from '../NewAccount'
+
+export default {
+  name: 'NewAccount',
+  components: { NewAccountContent },
+  data() {
+    return {
+      drawer: false,
+    }
+  },
+}
+</script>
+
+<style scoped></style>
