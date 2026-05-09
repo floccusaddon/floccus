6cc065db069ffa49462f7de66bf81813bd2ede27
Marcel Klehr <mklehr@gmx.net>
2026-05-09 17:57:07 +0200

    tests: Add regression test for confusing moved folders in Scanner
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- src/test/test.js
+++ src/test/test.js
@@ -4124,6 +4124,143 @@ describe('Floccus', function() {
                 false
               )
             })
+            it('should move items without confusing folders', async function() {
+              const localRoot = account.getData().localRoot
+
+              const folder1 = await browser.bookmarks.create({
+                title: 'a',
+                parentId: localRoot
+              })
+              const folder2 = await browser.bookmarks.create({
+                title: 'b',
+                parentId: folder1.id
+              })
+              const folder3 = await browser.bookmarks.create({
+                title: 'c',
+                parentId: folder2.id
+              })
+              const folder4 = await browser.bookmarks.create({
+                title: 'd',
+                parentId: localRoot
+              })
+              const folder5 = await browser.bookmarks.create({
+                title: 'e',
+                parentId: folder4.id,
+              })
+              const folder6 = await browser.bookmarks.create({
+                title: 'f',
+                parentId: folder5.id,
+              })
+              const folderX = await browser.bookmarks.create({
+                title: 'X',
+                parentId: folder3.id,
+              })
+              await browser.bookmarks.create({
+                title: 'url',
+                url: 'http://ur.l/',
+                parentId: folderX.id
+              })
+              const folderX2 = await browser.bookmarks.create({
+                title: 'X',
+                parentId: folder6.id,
+              })
+              await browser.bookmarks.create({
+                title: 'test',
+                url: 'http://urrr.l/',
+                parentId: folderX2.id
+              })
+              const test2Bm = await browser.bookmarks.create({
+                title: 'test2',
+                url: 'http://urrr2.l/',
+                parentId: folder1.id
+              })
+
+              await account.sync() // propagate to server
+              expect(account.getData().error).to.not.be.ok
+
+              await account.sync() // make sure order is propagated
+              expect(account.getData().error).to.not.be.ok
+
+              await account.init() // remove cache
+
+              await browser.bookmarks.move(folderX.id, { parentId: folder2.id })
+              await browser.bookmarks.move(folderX2.id, { parentId: folder5.id })
+              await browser.bookmarks.move(test2Bm.id, { parentId: folderX.id })
+
+              await account.sync() // propagate to server
+              expect(account.getData().error).to.not.be.ok
+
+              const tree = await getAllBookmarks(account)
+              expectTreeEqual(
+                tree,
+                new Folder({
+                  title: tree.title,
+                  children: [
+                    new Folder({
+                      title: 'a',
+                      children: [
+                        new Folder({
+                          title: 'b',
+                          children: [
+                            new Folder({
+                              title: 'c',
+                              children: [],
+                            }),
+                            new Folder({
+                              title: 'X',
+                              children: [
+                                new Bookmark({
+                                  title: 'url',
+                                  url: 'http://ur.l/',
+                                }),
+                                new Bookmark({
+                                  title: 'test2',
+                                  url: 'http://urrr2.l/',
+                                }),
+                              ],
+                            }),
+                          ],
+                        }),
+                      ],
+                    }),
+                    new Folder({
+                      title: 'd',
+                      children: [
+                        new Folder({
+                          title: 'e',
+                          children: [
+                            new Folder({
+                              title: 'f',
+                              children: [],
+                            }),
+                            new Folder({
+                              title: 'X',
+                              children: [
+                                new Bookmark({
+                                  title: 'test',
+                                  url: 'http://urrr.l/',
+                                }),
+                              ],
+                            }),
+                          ],
+                        }),
+                      ],
+                    }),
+                  ],
+                }),
+                false,
+                false
+              )
+
+              const localTree = await account.localTree.getBookmarksTree(true)
+              localTree.title = tree.title
+              expectTreeEqual(
+                localTree,
+                tree,
+                false,
+                false
+              )
+            })
           })
         })
         context('with two clients', function() {
