06fc7a6969622e4a7b26ebe785de255bbd7be36c
Marcel Klehr <mklehr@gmx.net>
2026-06-11 23:17:27 +0200

    fix: Allow uncaught exceptions in test-node.js
    in order to not fail in interrupt tests
    
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- src/entries/test-node.js
+++ src/entries/test-node.js
@@ -49,6 +49,10 @@ process.on('unhandledRejection', (reason, promise) => {
   console.error('Unhandled Rejection at:', promise, 'reason:', reason)
 })
 
+process.on('uncaughtException', (error) => {
+  console.error('Uncaught Exception:', error)
+})
+
 async function main() {
   const mocha = new Mocha({
     color: true,
