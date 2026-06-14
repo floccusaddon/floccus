500f6af211d13c956d584acfe76910d19bc8c7d4
Marcel Klehr <mklehr@gmx.net>
2026-06-14 15:59:22 +0200

    fix(tests): Eliminate randomness in yieldToEventLoop
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- src/lib/yieldToEventLoop.ts
+++ src/lib/yieldToEventLoop.ts
@@ -1,3 +1,10 @@
+import { isTest } from './isTest'
+
 export async function yieldToEventLoop() {
+  if (isTest) {
+    // Eliminate randomness in tests by not yielding to the event loop, which can cause tests to run in a different order
+    return Promise.resolve()
+  }
+  // In production we need this to prevent the browser from killing the background worker
   await new Promise(resolve => setTimeout(resolve, 0))
 }
