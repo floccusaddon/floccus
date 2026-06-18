e97265dc7859a37201e37d3e01a97001f361f464
Marcel Klehr <mklehr@gmx.net>
2026-06-18 22:19:18 +0200

    fix(yieldToEventLoop): Only eliminate randomness when running in node.js
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- src/lib/yieldToEventLoop.ts
+++ src/lib/yieldToEventLoop.ts
@@ -1,8 +1,6 @@
-import { isTest } from './isTest'
-
 export async function yieldToEventLoop() {
-  if (isTest) {
-    // Eliminate randomness in tests by not yielding to the event loop, which can cause tests to run in a different order
+  if (process && process.versions.node) {
+    // Eliminate randomness in node.js tests by not yielding to the event loop, which can cause tests to run in a different order
     return Promise.resolve()
   }
   // In production we need this to prevent the browser from killing the background worker
